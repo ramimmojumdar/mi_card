162c333fd269250887d51552d2d139d95d9ec3a7
ramimmojumdar <expertramim@gmial.com>
2024-02-15 14:41:11 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -12,78 +12,76 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Center(
-          child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/mojumdar.jpg'),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/mojumdar.jpg'),
+              ),
+              Text(
+                "Ramim Mojumdar",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico"
                 ),
-                Text(
-                  "Md. Ramim Mojumdar",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "Pacifico"
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 20.0,
+                  letterSpacing: 2.4,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Source Code Pro",
+
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 100.0,
+                child: Divider(
+                  color: Colors.teal.shade50,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal:20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mobile_friendly,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+880 13 120 839 40",
+                    style: TextStyle(
+                      fontSize: 20,
+                      // fontFamily: "Source Code Pro",
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
                 ),
-                Text(
-                  "FLUTTER DEVELOPER",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 20.0,
-                    letterSpacing: 2.4,
-                    fontFamily: "Source Code Pro",
+              ),
+              Card(
+                margin:EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "expertramim@gmail.com",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
 
                   ),
                 ),
-                SizedBox(
-                  height: 20,
-                  width: 100.0,
-                  child: Divider(
-                    color: Colors.teal.shade50,
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(vertical: 20, horizontal:70),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.mobile_friendly,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "+880 13 120 839 40",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "Source Code Pro",
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                Card(
-                  margin:EdgeInsets.symmetric(vertical: 0, horizontal: 70),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "expertramim@gmail.com",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "Source Code Pro",
-                        fontWeight: FontWeight.bold,
-                      ),
-
-                    ),
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
